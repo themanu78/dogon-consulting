a3b9858414ced4020da8cd2f56ba87d2547d2b58
manuel audrain <manuel.audrain@free.fr>
2026-01-14 18:12:28 +0100

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/dogon-consulting",
-  //base: "/",
+import dotenv from "dotenv";
+dotenv.config();
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: "/" + env.VITE_PUBLIC_BASE,
+  };
 });
+
+//export default defineConfig({
+//  plugins: [react()],
+// base: "/dogon-consulting",
+//base: import.meta.env.VITE_PUBLIC_BASE,
+//base: "/",
+//});
