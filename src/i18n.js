b305eec68641d5a9e288dd0cee9d20e35f6bc6bd
manuel audrain <manuel.audrain@free.fr>
2026-01-14 11:30:41 +0100

    ok

--- src/i18n.js
+++ src/i18n.js
@@ -18,6 +18,9 @@ i18n
   .init({
     fallbackLng: "en",
     debug: true,
+    backend: {
+      loadPath: "/locales/{{lng}}/translation.json",
+    },
   });
 export const languages = ["fr", "de", "en"];
 
